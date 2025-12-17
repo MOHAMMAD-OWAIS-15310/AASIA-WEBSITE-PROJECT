1391aff7c465ea863f898d3fbe5023d8f8114e3a
MOHAMMAD-OWAIS-15310 <1234owaismohd.12@gmail.com>
2025-12-17 21:54:19 +0530

    added notify button for not available

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,39 @@ console.log(paintings);
 
 const container = document.getElementById("cards-container");
 
-paintings.forEach(p => {
+// paintings.forEach(p => {
+//     const card = `
+//         <main>
+//             <div class="parent">
+//                 <a href="one_img.html">
+//                 <img src="${p.image}" alt="" height="200px" class="g" >
+//                 </a>
+//                 <div class="b">COST - ${p.price} RS</div>
+//                 <div class="c">SIZE - ${p.size}</div>
+//                 <div class="e">MEDIUM - ${p.medium.toUpperCase()}</div>
+//                 <div class="f">
+//                     AVAILABLE ? 
+//                     <span class="${p.Available === 'YES' ? 'available-yes' : 'available-no'}">
+//                         ${p.Available}
+//                     </span>
+//                 </div>
+
+                
+//                 <div class="d">
+//                     <button class="de">Buy Now</button>
+//                     <button class="de">Add to Cart</button>
+//                 </div>
+//             </div>
+//         </main>
+//     `;
+
+//     container.innerHTML += card;
+// });
+
+
+
+
+for(let p of paintings){
     const card = `
         <main>
             <div class="parent">
@@ -19,15 +51,25 @@ paintings.forEach(p => {
                         ${p.Available}
                     </span>
                 </div>
+                <div class="d">
+                   <div class="d">
+                    ${
+                        p.Available === "YES"
+                        ? `
+                            <button class="de">Buy Now</button>
+                            <button class="de">Add to Cart</button>
+                          `
+                        : `
+                            <button class="de notify">Notify Me</button>
+                          `
+                    }
+                </div>
+                </div>
 
                 
-                <div class="d">
-                    <button class="de">Buy Now</button>
-                    <button class="de">Add to Cart</button>
-                </div>
             </div>
         </main>
     `;
 
     container.innerHTML += card;
-});
+};
